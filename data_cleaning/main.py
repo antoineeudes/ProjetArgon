1abccd03d3774e5f73256b854b1a7ed6ad0aaa42
Antoine <antoine.eudes@gmail.com>
2019-03-18 18:00:39 +0100

    Update main.py

--- data_cleaning/main.py
+++ data_cleaning/main.py
@@ -6,18 +6,20 @@ def copy(path):
     data = pd.read_csv('../data/data_raw/Articles.csv')
     data.to_csv('../data/data_cleaned/'+path)
 
-def remove_spaces_in_column_names():
+def remove_spaces_and_parentheses_in_column_names():
     file_paths = ["Articles.csv", "Sales.csv", "Location.csv", 'Market_Data.csv', "Stock.csv"]
     for file_path in file_paths:
         data = pd.read_csv('../data/data_cleaned/'+file_path)
         column_names = data.columns.tolist()
         for column_name in column_names:
             if ' ' in column_name:
-                data = data.rename(columns={column_name: '_'.join(column_name.split(' '))})
+                data = data.rename(columns={column_name: '_'.join(column_name.split(' ')).replace('(', '').replace(')', '')})
         data.to_csv("../data/data_cleaned/"+file_path, index=False, encoding='utf8')
+
+
 clean_articles()
 clean_locations()
 copy('Sales.csv')
 copy('Stock.csv')
 copy('Market_Data.csv')
-remove_spaces_in_column_names()
+remove_spaces_and_parentheses_in_column_names()
