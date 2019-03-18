b0f094b0596a59149d82843c9ff3c32e4bc402bb
Antoine <antoine.eudes@gmail.com>
2019-03-18 18:44:28 +0100

    added prints to explain operations

--- data_cleaning/main.py
+++ data_cleaning/main.py
@@ -4,13 +4,16 @@ from stock_cleaning import clean_stock
 import pandas as pd
 
 def copy(path):
+    
     data = pd.read_csv('../data/data_raw/'+path)
     data.to_csv('../data/data_cleaned/'+path, index=False, encoding='utf8')
 
 def remove_spaces_and_parentheses_in_column_names():
+    print("removing spaces and parentheses...")
     file_paths = ["Articles.csv", "Sales.csv", "Location.csv", 'Market_Data.csv']
 
     for file_path in file_paths:
+        print("file "+file_path)
         data = pd.read_csv('../data/data_cleaned/'+file_path)
         column_names = data.columns.tolist()
         for column_name in column_names:
@@ -18,13 +21,13 @@ def remove_spaces_and_parentheses_in_column_names():
                 data = data.rename(columns={
                         column_name: '_'.join(column_name.split(' ')).replace('(', '').replace(')', '')
                 })
-            data.to_csv("../data/data_cleaned/"+file_path, index=False, encoding='utf8')
+        data.to_csv("../data/data_cleaned/"+file_path, index=False, encoding='utf8')
+        print("Done.")
 
 
 clean_articles()
 clean_locations()
 clean_stock()
-# copy('Sales.csv')
-# copy('Stock.csv')
-# copy('Market_Data.csv')
+copy('Sales.csv')
+copy('Market_Data.csv')
 remove_spaces_and_parentheses_in_column_names()
