965c1ca0c9e814bb639c83997249ce9d27456b97
Unknown <alexperez@wanadoo.fr>
2019-03-18 17:43:42 +0100

    Added Location_MarketData join

--- data_cleaning/make_joins.py
+++ data_cleaning/make_joins.py
@@ -1,23 +1,36 @@
 import pandas as pd
+############################################################################
+# Execute this file to make all possible joins of the tables in input_path #
+############################################################################
+
+input_path = '../data/data_raw/'
+output_path = '../data/data_cleaned/'
+
 
 def delete_column_with_regex(df, regex):
     df = df[df.columns.drop(list(df.filter(regex=regex)))]
     return df
 
-data_path = '../data/data_raw/'
-Location = pd.read_csv(data_path+'Location.csv')
+# Load all tables and set primary index if it exists
+print('\nLoading csv...\nLocation.csv')
+Location = pd.read_csv(input_path+'Location.csv')
 Location.set_index('Location (Code)', verify_integrity=True)
 # Articles = pd.read_csv('data/Articles.csv')
-# Market_Data = pd.read_csv('data/Market_Data.csv')
-Sales = pd.read_csv(data_path+'Sales.csv')
+print('Market_Data.csv')
+Market_Data = pd.read_csv(input_path+'Market_Data.csv')
+print('Sales.csv\n')
+Sales = pd.read_csv(input_path+'Sales.csv')
 # Stock = pd.read_csv('data/Stock.csv')
 
-print(Location)
 
-# columns_to_use = Location.columns.difference(Sales.columns)
 # Jointures :
 Location_Sales = Sales.merge(Location, on=['Location (Code)'], how='outer', suffixes=('', '_to_delete'))
 Location_Sales = delete_column_with_regex(Location_Sales, '_to_delete')
 
-Location_Sales.to_csv("../data/data_cleaned/Location_Sales.csv",index=False)
-print(Location_Sales)
+Location_MarketData = Market_Data.merge(Location, on=['Location (Code)'], how='outer', suffixes=('', '_to_delete'))
+Location_MarketData = delete_column_with_regex(Location_MarketData, '_to_delete')
+
+# Save joins
+print('Saving...')
+Location_Sales.to_csv(output_path+'/Location_Sales.csv',index=False)
+Location_MarketData.to_csv(output_path+'Location_MarketData.csv',index=False)
