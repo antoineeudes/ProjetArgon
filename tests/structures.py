b324575c54967bfbdfe38e9b474781f5d95928de
Unknown <alexperez@wanadoo.fr>
2019-03-22 12:07:27 +0100

    Added test on demand class

--- tests/structures.py
+++ tests/structures.py
@@ -3,6 +3,7 @@ import unittest
 import sys
 sys.path.append("..")
 from structure.stock import Stock
+from structure.demand import Demand
 
 
 class StockTest(unittest.TestCase):
@@ -14,6 +15,27 @@ class StockTest(unittest.TestCase):
         stock.define("ZDB", "AME", "2016-01-03T00:00:00.000Z", 10)
         self.assertEqual(stock.state["2016-01-02T00:00:00.000Z"]["UBADP"]["ZDB"], 5)
         self.assertEqual(len(stock.state), 2)
+
+class DemandTest(unittest.TestCase):
+
+    def test_set_demand_range(self):
+        demand = Demand()
+        demand.set_demand_range_LA(5, 10, 'ZDB', 'AME', 3)
+
+        self.assertEqual(demand.get_demand_DLA(5, 'ZDB', 'AME'), 3)
+        self.assertEqual(demand.get_demand_DLA(9, 'ZDB', 'AME'), 3)
+        self.assertEqual(demand.get_demand_DLA(10, 'ZDB', 'AME'), 0)
+
+    def test_edit_existing_demand(self):
+        demand = Demand()
+        demand.set_demand_range_LA(5, 10, 'ZDB', 'AME', 3)
+        demand.set_demand_range_LA(7, 8, 'ZDB', 'AME', 2)
+
+        self.assertEqual(demand.get_demand_DLA(5, 'ZDB', 'AME'), 3)
+        self.assertEqual(demand.get_demand_DLA(7, 'ZDB', 'AME'), 2)
+        self.assertEqual(demand.get_demand_DLA(8, 'ZDB', 'AME'), 3)
+        self.assertEqual(demand.get_demand_DLA(10, 'ZDB', 'AME'), 0)
+
 
 if __name__=="__main__":
     unittest.main()
