4d7deef7758d9499e3e3f2bb9a1ba2da1f180dff
Unknown <alexperez@wanadoo.fr>
2019-04-08 16:10:37 +0200

    transform_features.py

--- algo/demand_forecasting/random_forest/transform_features.py
+++ algo/demand_forecasting/random_forest/transform_features.py
@@ -1,1 +1,10 @@
-import panda as pd
+import pandas as pd
+
+input_path = '../../../data/data_cleaned/'
+
+Dataframe = pd.read_csv(input_path+'Sales_Articles_Location.csv')
+
+X = []
+
+Locations = pd.read_csv(input_path+'Location.csv')
+print(Locations['Location_Code'])
