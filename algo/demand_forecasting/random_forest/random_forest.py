979305e29be086fbe286c0ec0622543693ab240e
Antoine <antoine.eudes@gmail.com>
2019-04-14 17:45:52 +0200

    Update random_forest.py

--- algo/demand_forecasting/random_forest/random_forest.py
+++ algo/demand_forecasting/random_forest/random_forest.py
@@ -1,12 +1,23 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-def trainRandomForest(input_data, output_data, test_proportion=0.4, maxdepth=1000):
+def trainRandomForest(input_data, output_data, test_proportion=0.3, maxdepth=1000):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_proportion)
-    clf = RandomForestRegressor(max_depth=max_depth)
+    clf = RandomForestRegressor(max_depth=maxdepth)
     clf.fit(X_train, y_train)
+    print("score:", clf.score(X_test, y_test))
     return clf
 
 
 def predictDemand(X_test, clf):
     return clf.predict(X_test)
+
+
+if __name__=='__main__':
+    print("reading csv...")
+    dataframe = pd.read_csv('../../../data/data_cleaned/RandomForest_X.csv')
+    X = dataframe.iloc[:, :-1]
+    y = dataframe['y']
+    print('training random forest...')
+    clf = trainRandomForest(X, y)
