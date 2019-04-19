8bf5dc3514df1215e03216651a134edfc2c30d50
Unknown <alexperez@wanadoo.fr>
2019-04-19 19:52:52 +0200

    Cleaned code

--- algo/demand_forecasting/random_forest/transform_features2.py
+++ algo/demand_forecasting/random_forest/transform_features2.py
@@ -38,21 +38,6 @@ def datetime_to_range_year(datetime, period_length):
 
     return day_index//period_length, year
 
-# def compute(ns, start_index, length=None, i=None):
-#     print('Starting process {}'.format(i))
-#     df = ns.df
-#     # k = 0
-#     for index, row in islice(df.iterrows(), start_index, None):
-#         datetime = date.fromisoformat(row[date_key])
-#         period_number, year = datetime_to_range_year(datetime, period_length)
-#         df.at[index, period_key] = period_number
-#         df.at[index, year_key] = year
-#         df.at[index, 'Y'] = get_y(row[item_key], row[location_key], row[date_key])
-#         print(i*'\t'+str(index))
-#         # k += 1
-#         # if length != None and k >= length :
-#         #     break
-
 def compute(df, i=None):
     print('Starting process {}'.format(i))
     for index, row in df.iterrows():
@@ -61,6 +46,7 @@ def compute(df, i=None):
         df.at[index, period_key] = period_number
         df.at[index, year_key] = year
         df.at[index, 'Y'] = get_y(row[item_key], row[location_key], row[date_key])
+        # if index % (df.shape[0]//1000) == 0:
         print(i*'\t'+str(index))
 
     return df
@@ -78,42 +64,12 @@ def compute_X(save = False):
     df[period_key] = 0
     df[year_key] = 0
 
-    # print(df)
-
-    # def compute(dataframe, start_index, length=None):
-    #     print('compute')
-    #     k = 0
-    #     for index, row in islice(df.iterrows(), start_index, None):
-    #         datetime = date.fromisoformat(row[date_key])
-    #         period_number, year = datetime_to_range_year(datetime, period_length)
-    #         df.at[index, period_key] = period_number
-    #         df.at[index, year_key] = year
-    #         df.at[index, 'Y'] = get_y(row[item_key], row[location_key], row[date_key])
-    #         print(index)
-    #         k += 1
-    #         if k != None and k >= length :
-    #             break
-
-    # compute(2, 10)
-    # compute(0, 2)
-
     p = multiprocessing.Pool(processes = multiprocessing.cpu_count()-1)
-    #timing it...
-    # start = time.time()
 
     nb_rows = df.shape[0]
     nb_partition = multiprocessing.cpu_count()-1
-    partition_width = 1000
-    # partition_width = nb_rows//nb_partition
-    print(nb_partition)
+    partition_width = nb_rows//nb_partition
 
-    # args = []
-    # for i in range(nb_partition-1):
-    #     args.append((ns, partition_width*i, partition_width, i))
-    # args.append((ns, partition_width*(nb_partition-1), partition_width, nb_partition-1))
-    # args.append((df, partition_width*(nb_partition-1), None, nb_partition))
-    #     p.apply_async(compute, [partition_width*i, partition_width])
-    # p.apply_async(compute, [partition_width*(nb_partition-1), None])
     args = []
     for i in range(nb_partition):
         args.append((df.iloc[partition_width*i:partition_width*(i+1), :], i))
@@ -121,22 +77,14 @@ def compute_X(save = False):
     results = p.starmap(compute, args)
     df = pd.concat(results)
 
-    # df = compute(df.iloc[:partition_width, :], 1)
-
     p.close()
     p.join()
 
     df.drop([date_key], axis=1, inplace=True)
 
-    # print(df)
-
-    # print(df.loc[0])
-    # print(df.loc[1])
-
     encoder = ce.BinaryEncoder(cols=[location_key, item_key])
     df = encoder.fit_transform(df)
 
-    # print(df.loc[0])
     print(df)
     if save:
         df.to_csv(output_path+'RandomForest_X.csv',index=False)
