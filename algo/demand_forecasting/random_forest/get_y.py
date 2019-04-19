08d185902df7fd47c4bc58557ca0dcbbe5ca7672
Unknown <alexperez@wanadoo.fr>
2019-04-19 16:12:58 +0200

    Fixed issue with get_y

--- algo/demand_forecasting/random_forest/get_y.py
+++ algo/demand_forecasting/random_forest/get_y.py
@@ -9,14 +9,15 @@ def get_y(item_code, location_code, date):
     # dataframe = dataframe.loc(([dataframe['Item_Code']==item_code]) & (dataframe['Location_Code']==location_code) & (dataframe['Day_in_year_YYYYMMDD']==date))
     # dataframe = dataframe[dataframe['Location_Code']==location_code]
     # dataframe = dataframe[dataframe['Day_in_year_YYYYMMDD']==date]
-    dataframe = dataframe[dataframe['Item_Code']==item_code]
-    dataframe = dataframe[dataframe['Location_Code']==location_code]
-    dataframe = dataframe[dataframe['Day_in_year_YYYYMMDD']==date]
-
+    df = dataframe[dataframe['Item_Code']==item_code]
+    df = df[df['Location_Code']==location_code]
+    df = df[df['Day_in_year_YYYYMMDD']==date]
+    
     s = 0
-    for index, row in dataframe.iterrows():
+    for index, row in df.iterrows():
         s+= int(row["Sales_units"])
     return s
 
 if __name__=='__main__':
     print(get_y('SLT', 'MCA', '2016-04-15'))
+    print(get_y('SLT', 'MCA', '2016-09-06'))
