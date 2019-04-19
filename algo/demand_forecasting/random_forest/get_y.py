e8de9aa4d2c96916bbba93687279ca73830eda24
Unknown <alexperez@wanadoo.fr>
2019-04-19 21:26:30 +0200

    One query get_y

--- algo/demand_forecasting/random_forest/get_y.py
+++ algo/demand_forecasting/random_forest/get_y.py
@@ -6,17 +6,14 @@ file_test = '../../../data/data_cleaned/Sales_MarketData.csv'
 dataframe = pd.read_csv(file_test)
 def get_y(item_code, location_code, date):
     global dataframe
-    # dataframe = dataframe.loc(([dataframe['Item_Code']==item_code]) & (dataframe['Location_Code']==location_code) & (dataframe['Day_in_year_YYYYMMDD']==date))
-    # dataframe = dataframe[dataframe['Location_Code']==location_code]
-    # dataframe = dataframe[dataframe['Day_in_year_YYYYMMDD']==date]
-    df = dataframe[dataframe['Item_Code']==item_code]
-    df = df[df['Location_Code']==location_code]
-    df = df[df['Day_in_year_YYYYMMDD']==date]
-    
+    df = dataframe.query('Item_Code == @item_code & Location_Code == @location_code & Day_in_year_YYYYMMDD == @date')
+
     s = 0
     for index, row in df.iterrows():
         s+= int(row["Sales_units"])
+
     return s
+
 
 if __name__=='__main__':
     print(get_y('SLT', 'MCA', '2016-04-15'))
