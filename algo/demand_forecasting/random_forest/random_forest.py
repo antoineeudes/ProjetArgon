7d8468627afb785e35c249345ac5ce953f9aed95
Unknown <alexperez@wanadoo.fr>
2019-04-21 17:12:13 +0200

    Cleaned code

--- algo/demand_forecasting/random_forest/random_forest.py
+++ algo/demand_forecasting/random_forest/random_forest.py
@@ -39,17 +39,4 @@ def trainRandomForest_on(filename, test_proportion=0.3, maxdepth=1000):
     return clf
 
 if __name__=='__main__':
-    # print("reading csv...")
-    # dataframe = pd.read_csv('../../../data/data_cleaned/XY_7.csv')
-    # X = dataframe.iloc[:, :-1]
-    # y = dataframe['Y']
-    # try:
-    #     print('loading moel')
-    #     clf = pickle.load(open(model_filename, 'rb'))
-    # except:
-    #     print('training random forest...')
-    #     clf = trainRandomForest(X, y)
-    # clf.score()
-    # print(clf.predict([[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,105,2017], [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,192,2017]]))
     clf = trainRandomForest_on('XY_stockbased_7.csv')
-    # print("score:", clf.score(X_test, y_test))
